6e26358df0b87d1f018b9ee31336e4db17c2ebb3
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-09-03 21:20:22 +0530

    ✅ Buttons,icons,margin,padding,row,column,images and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,123 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      // backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: const Text("My First App"),
+        title: const Text("First App"),
         centerTitle: true,
         backgroundColor: Colors.indigo[700],
       ),
-      body: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.normal,
-              fontFamily: 'Roboto',
-              color: Colors.white),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            color: Colors.red.shade500,
+            padding: const EdgeInsets.all(10.0),
+            child: const Text('Box'),
+          ),
+          Container(
+            color: Colors.green.shade500,
+            padding: const EdgeInsets.all(20.0),
+            child: const Text('Box'),
+          ),
+          Container(
+            color: Colors.orange.shade500,
+            padding: const EdgeInsets.all(30.0),
+            child: const Text('Box'),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
         backgroundColor: Colors.indigo[700],
-        child: const Text('Click'),
+        child: Icon(
+          Icons.search,
+          color: Colors.indigo[100],
+        ),
       ),
     );
   }
 }
+
+
+//--------------------------------------Column
+
+// Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.end,
+//         children: <Widget>[
+//           Container(
+//             color: Colors.red.shade500,
+//             padding: const EdgeInsets.all(10.0),
+//             child: const Text('Box'),
+//           ),
+//           Container(
+//             color: Colors.green.shade500,
+//             padding: const EdgeInsets.all(20.0),
+//             child: const Text('Box'),
+//           ),
+//           Container(
+//             color: Colors.orange.shade500,
+//             padding: const EdgeInsets.all(30.0),
+//             child: const Text('Box'),
+//           ),
+//         ],
+//       ),
+
+//-----------------------------------------Row
+
+// Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           Container(
+//             color: Colors.red.shade500,
+//             padding: const EdgeInsets.all(10.0),
+//             child: const Text('Box'),
+//           ),
+//           Container(
+//             color: Colors.green.shade500,
+//             padding: const EdgeInsets.all(20.0),
+//             child: const Text('Box'),
+//           ),
+//           Container(
+//             color: Colors.orange.shade500,
+//             padding: const EdgeInsets.all(30.0),
+//             child: const Text('Box'),
+//           ),
+//         ],
+//       ),
+
+//------------------------------------Buttons
+
+// child: ElevatedButton.icon(
+//         icon: const Icon(Icons.search),
+//         label: const Text(
+//           'Search',
+//           style: TextStyle(fontSize: 20),
+//         ),
+//         onPressed: () => {},
+//         style: ButtonStyle(
+//           backgroundColor: MaterialStateProperty.all(Colors.indigo[700]),
+//         ),
+//       )
+
+
+// child: IconButton(
+//         icon: const Icon(
+//           Icons.flutter_dash_sharp,
+//         ),
+//         iconSize: 100,
+//         color: Colors.blue.shade400,
+//         onPressed: () => {},
+//       )
+
+//--------------------------------------Margin pading and container
+
+//  Container(
+//         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+//         margin: const EdgeInsets.all(10.0),
+//         color: Colors.grey.shade400,
+//         child: const Text('hello world'),
+//       )
